21ba5a88376b6fea0e5aff2e3579a1f6622b610a
Muhammad Amsyal Ikhsan <muhammadamsyal@gmail.com>
2024-11-22 13:36:58 +0700

    update operator

--- kalkulator.js
+++ kalkulator.js
@@ -1,21 +1,40 @@
 const calculateBtn = document.getElementById("calculateBtn");
 
 calculateBtn.addEventListener("click", () => {
-  let userInput = document.getElementById("userInput").value;
+  let userInput = document.getElementById("userInput").value.toLowerCase();
+  console.log(userInput);
   let kata = userInput.split(" ");
+
+  let result;
+  let kalimatBaru;
 
   for (let i = 0; i < kata.length; i++) {
     if (kata[i] == "and") {
       kata[i] = "&&";
+      kalimatBaru = kata.join(" ");
+      result = eval(kalimatBaru);
     } else if (kata[i] == "or") {
       kata[i] = "||";
+      kalimatBaru = kata.join(" ");
+      result = eval(kalimatBaru);
     } else if (kata[i] == "not") {
       kata[i] = "!";
+      kalimatBaru = kata.join(" ");
+      result = eval(kalimatBaru);
+    } else if (kata[i] == "nand") {
+      kata[i] = "&&";
+      kalimatBaru = kata.join(" ");
+      result = !eval(kalimatBaru);
+    } else if (kata[i] == "nor") {
+      kata[i] = "||";
+      kalimatBaru = kata.join(" ");
+      result = !eval(kalimatBaru);
+    } else if (kata[i] == "xor") {
+      kata[i] = "!==";
+      kalimatBaru = kata.join(" ");
+      result = eval(kalimatBaru);
     }
   }
-
-  let kalimatBaru = kata.join(" ");
-  let result = eval(kalimatBaru);
 
   document.getElementById("result").textContent = result;
 });
